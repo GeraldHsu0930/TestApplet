4c81c180dc69572f30e955a30cc3cb31231fe337
GeraldHsu0930 <geraldhsu@pro-view.com.tw>
2025-05-19 14:31:54 +0800

    Add files via upload

---
+++ TestApplet.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package com.proview.test;
+
+import javacard.framework.Applet;
+import javacard.framework.ISOException;
+import javacard.framework.ISO7816;
+import javacard.framework.APDU;
+
+/**
+ * @author Tsengi7
+ *
+ */
+public class TestApplet extends Applet {
+	public static void install(byte[] bArray, short bOffset, byte bLength) {
+		// GP-compliant JavaCard applet registration
+		new com.proview.test.TestApplet().register(bArray,
+				(short) (bOffset + 1), bArray[bOffset]);
+	}
+
+	public void process(APDU apdu) {
+		// Good practice: Return 9000 on SELECT
+		if (selectingApplet()) {
+			return;
+		}
+
+		byte[] buf = apdu.getBuffer();
+		switch (buf[ISO7816.OFFSET_INS]) {
+		case (byte) 0x00:
+			break;
+		default:
+			// good practice: If you don't know the INStruction, say so:
+			ISOException.throwIt(ISO7816.SW_INS_NOT_SUPPORTED);
+		}
+	}
+}
